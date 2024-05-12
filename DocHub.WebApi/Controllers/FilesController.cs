33bac8ea0939949307b9faf72542167d94c2a9e1
srht <srhtkrgl@gmail.com>
2024-05-13 00:48:06 +0300

    fileservice

---
+++ DocHub.WebApi/Controllers/FilesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DocHub.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FilesController : ControllerBase
+    {
+    }
+}
